2225607b7cbc33351329c992da985ada8319135b
marcelobarbieri <marcelodpbarbieri@gmail.com>
2022-02-12 14:48:10 -0300

    C# Parse

---
+++ parse/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Globalization;
+
+// Parse
+
+var valor1 = int.Parse("123");
+var valor2 = bool.Parse("true");
+var valor3 = DateTime.Parse("01/01/2022");
+
+Console.WriteLine(valor1); // 123
+Console.WriteLine(valor2); // True
+Console.WriteLine(valor3); // 01/01/2022 00:00:00
+
+// IFormatProvider
+
+var valor4 = DateTime.Parse("28/10/2022");
+Console.WriteLine(valor4);
+
+var culture = new CultureInfo("en-US");
+Console.WriteLine(culture); // pt-BR
+var valor5 = DateTime.Parse("10/28/2022", culture);
+Console.WriteLine(valor5);
+
+// NumberStyles
+
+Console.WriteLine(int.Parse("1234", NumberStyles.None)); // 1234
+Console.WriteLine(int.Parse("12123,0", NumberStyles.AllowDecimalPoint)); // 12123
+Console.WriteLine(int.Parse("123.456", NumberStyles.AllowThousands));  // 12356
+
+// TryParse
+
+var deuCerto1 = int.TryParse("45689", out var result1);
+Console.WriteLine(deuCerto1); // True
+Console.WriteLine(result1); // 45689
+
+var deuCerto2 = int.TryParse("asdaasd", out var result2);
+Console.WriteLine(deuCerto2); // False
+Console.WriteLine(result2); // 0
+
---
+++ parse/README.md
@@ -0,0 +1,9 @@
+# Parse e TryParse no C#
+
+[Balta.io](https://balta.io/blog/csharp-parse-tryparse)
+
+Todos os tipos **built-in** do .NET possuem esses métodos.
+
+> CultureInfo
+
+> NumberStyles
